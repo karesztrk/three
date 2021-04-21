d61b4b756fdb3cc2a4cdfd6973b30dbdd61be673
Károly Török <torok.karoly.krisztian@gmail.com>
2021-04-21 09:50:08 +0200

    Planet rotation axis fix

--- universe/src/Mars.tsx
+++ universe/src/Mars.tsx
@@ -1,12 +1,14 @@
 import { useFrame, useLoader } from '@react-three/fiber';
 import React, { FC, useRef } from 'react';
-import { Mesh, TextureLoader } from 'three';
+import { Mesh, TextureLoader, Vector3 } from 'three';
 const Mars: FC = () => {
   const planet = useRef<Mesh>();
   const moon = useRef<Mesh>();
   const radius = 3;
   const position = 2;
-  const speed = 0.25;
+  const planetSpeed = 0.001;
+  const moonSpeed = 0.25;
+  const planetRotationAxis = new Vector3(0, 1, 0);
   const [colorMap, normalMap, iceworld] = useLoader(TextureLoader, [
     'mars_1k_color.jpg',
     'mars_1k_normal.jpg',
@@ -15,19 +17,20 @@ const Mars: FC = () => {
 
   useFrame(({ clock }) => {
     if (planet.current) {
-      planet.current.rotation.y = clock.getElapsedTime() * 0.05;
+      planet.current.rotateOnAxis(planetRotationAxis, planetSpeed);
     }
     if (moon.current) {
       moon.current.rotation.y = clock.getElapsedTime() * 0.25;
       moon.current.position.x =
-        Math.sin(clock.getElapsedTime() * speed) * radius + position;
+        Math.sin(clock.getElapsedTime() * moonSpeed) * radius + position;
+      Math.sin(clock.getElapsedTime() * moonSpeed) * radius + position;
       moon.current.position.z =
-        Math.cos(clock.getElapsedTime() * speed) * radius;
+        Math.cos(clock.getElapsedTime() * moonSpeed) * radius;
     }
   });
   return (
     <>
-      <mesh ref={planet} rotation={[0, 0, 0.5]} position={[2, 0, 0]}>
+      <mesh ref={planet} rotation={[0, 0, 0.25]} position={[2, 0, 0]}>
         <sphereGeometry args={[2, 64, 64]} />
         <meshStandardMaterial
           roughness={0.75}
