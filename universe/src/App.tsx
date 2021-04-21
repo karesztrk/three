74e54d34d04da38240378341cc4af286af6cf011
Károly Török <torok.karoly.krisztian@gmail.com>
2021-04-21 09:34:24 +0200

    Switched to Ortho camera

--- universe/src/App.tsx
+++ universe/src/App.tsx
@@ -1,16 +1,20 @@
 import { Canvas } from '@react-three/fiber';
 import React, { FC, Suspense } from 'react';
+import { OrthographicCamera } from 'three';
 import './App.css';
 import Scene from './Scene';
 
 interface AppProps {}
 
 const App: FC<AppProps> = () => {
+  const camera = new OrthographicCamera(-6, 6, 3, -3, -5, 5);
   return (
     <div className="App">
       <h1>Mars</h1>
       <h2>The red planet and its imaginary moon</h2>
       <Canvas
+        orthographic
+        camera={camera}
         style={{
           position: 'absolute',
         }}
