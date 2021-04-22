f02a46b8249b9a1c7d7879d8b37cb85ac2d40b19
Károly Török <torok.karoly.krisztian@gmail.com>
2021-04-22 18:44:00 +0200

    Switched back to Perspective camera

--- universe/src/App.tsx
+++ universe/src/App.tsx
@@ -7,14 +7,12 @@ import Scene from './Scene';
 interface AppProps {}
 
 const App: FC<AppProps> = () => {
-  const camera = new OrthographicCamera(-6, 6, 3, -3, -5, 5);
   return (
     <div className="App">
       <h1>Mars</h1>
       <h2>The red planet and its imaginary moon</h2>
       <Canvas
-        orthographic
-        camera={camera}
+        camera={{ fov: 60, near: 0.1, far: 10, position: [2, 0, 5] }}
         style={{
           position: 'absolute',
         }}
